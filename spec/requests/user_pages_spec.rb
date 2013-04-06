3c397ca84d5edc1d340818d2d8ecbb0599991230
Itay Klein <klein.itay@gmail.com>
2013-04-06 17:37:56 +0300

    stam

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -44,6 +44,29 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
+
+    describe "with invalid information" do
+      describe "after submission" do
+        before do
+          fill_in "Name", with: "Example User"
+          fill_in "Email", with: "user@example.com"
+          fill_in "Password", with: "foobar1"
+          fill_in "Confirmation", with: "foobar"
+          click_button submit
+        end
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+
+      end
     end
   end
 end
