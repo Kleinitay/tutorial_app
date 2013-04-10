8e362b4aa1cf3d237146779338dd0c3652f15ef3
Itay Klein <klein.itay@gmail.com>
2013-04-10 16:21:15 +0300

    finished microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,7 +18,7 @@ module SessionsHelper
   end
 
   def current_user?(user)
-      user == current_user
+    user == current_user
   end
 
   def sign_out
@@ -35,4 +35,10 @@ module SessionsHelper
     session[:return_to] = request.url
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
 end
